8573e9779e1773ea69d512b06b06ddaf19c3358a
Tobi Ogunsina <ogunsina04@gmail.com>
2026-02-24 12:55:24 -0600

    Added structure for frontend pages

---
+++ frontendweb/src/app/(platform)/learn/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// Pages in the app are async, this allows for server side rendering for better SEO
+const Page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default Page
